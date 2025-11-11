d7c33b376d99be74f4417e547b494701824193f2
Gabriel Alvarez Rosas <franklinalvarez1307005@gmail.com>
2025-11-11 09:30:39 -0500

    app funcional

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title=models.CharField(max_length=100, null=False)
+    director=models.CharField(max_length=100, null=False)
+    release_date=models.DateField(null=False)
+    genre=models.CharField(max_length=100, null=False)
+    synopsis=models.TextField(max_length=200, null=False)
+
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def movie(request, id:int):
+    movie = Movie.objects.get(id=id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie
+
+    }
+    return HttpResponse(template.render(context, request))
